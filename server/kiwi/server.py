8c171b6527f62a070a2342cac87db1b7921f16b6
Victor Nakoryakov <nail.xx@gmail.com>
2022-03-17 15:39:41 +0300

    feat(server): add specific door endpoint

--- server/kiwi/server.py
+++ server/kiwi/server.py
@@ -1,5 +1,5 @@
 import records
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 db = records.Database()
@@ -13,9 +13,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
-    return {"doors_url": "/doors/{?skip,limit}"}
+    return {
+        "doors_url": "/doors/{?skip,limit}",
+        "door_url": "/doors/{door_id}/",
+    }
+
 
 @app.get("/doors/")
 async def doors(skip: int = 0, limit: int = 10):
@@ -41,3 +46,48 @@ async def doors(skip: int = 0, limit: int = 10):
     )
 
     return rows.as_dict()
+
+
+@app.get("/doors/{door_id}/")
+async def doors(door_id: int):
+    door_row = db.query(
+        """
+        SELECT
+          doors.id,
+          doors.name,
+          addresses.street,
+          addresses.postal_code,
+          addresses.city,
+          addresses.state,
+          addresses.country_code
+        FROM doors
+        LEFT JOIN addresses
+        ON doors.address_id = addresses.id
+        WHERE doors.id = :door_id
+        """,
+        door_id=door_id,
+    ).first()
+
+    if not door_row:
+        raise HTTPException(status_code=404, detail="Door not found")
+
+    # Assuming a particular door is accessed by at most 100-200 people,
+    # it is OK to just list them all in the response without the need
+    # for a separate API call, pagination, etc.
+    user_rows = db.query(
+        """
+        SELECT
+          user_door_permissions.user_id,
+          users.email,
+          users.first_name,
+          users.last_name
+        FROM user_door_permissions
+        LEFT JOIN users
+        ON user_door_permissions.user_id = users.id
+        WHERE user_door_permissions.door_id = :door_id
+        ORDER BY users.last_name
+        """,
+        door_id=door_id,
+    )
+
+    return door_row.as_dict() | {"authorized_users": user_rows.as_dict()}
