433c7cd5a39c464985de1da9e68cd9a08b44b47f
Victor Nakoryakov <nail.xx@gmail.com>
2022-03-17 12:53:45 +0300

    feat: implement GET /doors/ endpoint

--- kiwi/server.py
+++ kiwi/server.py
@@ -6,5 +6,28 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    rows = db.query("SELECT * FROM doors")
+    return {"doors_url": "/doors/{?skip,limit}"}
+
+@app.get("/doors/")
+async def doors(skip: int = 0, limit: int = 10):
+    rows = db.query(
+        """
+        SELECT
+          doors.name,
+          addresses.street,
+          addresses.postal_code,
+          addresses.city,
+          addresses.state,
+          addresses.country_code
+        FROM doors
+        LEFT JOIN addresses
+        ON doors.address_id = addresses.id
+        ORDER BY doors.id
+        LIMIT :limit
+        OFFSET :offset
+        """,
+        offset=skip,
+        limit=limit,
+    )
+
     return rows.as_dict()
