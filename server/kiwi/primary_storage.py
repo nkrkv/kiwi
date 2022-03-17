249aa332413fb4c7dc3908e2e984a352e447be0f
Victor Nakoryakov <nail.xx@gmail.com>
2022-03-17 23:27:43 +0300

    fix(server): lat/lon confusion

--- server/kiwi/primary_storage.py
+++ server/kiwi/primary_storage.py
@@ -42,8 +42,8 @@ class Storage:
               addresses.city,
               addresses.state,
               addresses.country_code,
-              addresses.geolocation[0] as longitude,
-              addresses.geolocation[1] as latitude
+              addresses.geolocation[1] as longitude,
+              addresses.geolocation[0] as latitude
             FROM doors
             LEFT JOIN addresses
             ON doors.address_id = addresses.id
