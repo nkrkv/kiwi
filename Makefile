6d079400676359c7d48f75f64399cc35b4af4811
Victor Nakoryakov <nail.xx@gmail.com>
2022-03-17 12:32:03 +0300

    chore: scaffold database

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 
 .PHONY: dev
-dev:
-	pipenv run uvicorn kiwi.server:app --port 8100 --reload
+dev_server:
+	DATABASE_URL="postgresql://kiwi@localhost:5432/kiwi" \
+		pipenv run uvicorn kiwi.server:app --port 8100 --reload
--- kiwi/server.py
+++ kiwi/server.py
@@ -1,7 +1,10 @@
+import records
 from fastapi import FastAPI
 
+db = records.Database()
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    rows = db.query("SELECT * FROM doors")
+    return rows.as_dict()
